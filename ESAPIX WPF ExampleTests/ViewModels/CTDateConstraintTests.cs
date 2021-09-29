68cd91de893ec90fa718837be0bd29719c8c454b
Guozhen Luo <guozhen.luo@vumc.org>
2021-09-29 10:13:45 -0700

    add uni test. didn't copy over the DLL files

---
+++ ESAPIX WPF ExampleTests/ViewModels/CTDateConstraintTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ESAPX_StarterUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESAPIX.Constraints;
+
+namespace ESAPX_StarterUI.ViewModels.Tests
+{
+    [TestClass()]
+    public class CTDateConstraintTests
+    {
+        [TestMethod()]
+        public void ConstrainTest()
+        {
+            //Arrange
+            var ctDate = DateTime.Now.AddDays(-59);
+            //Act
+            var actual = new CTDateConstraint().Constrain(ctDate); ResultType;
+            //Assert
+            var expected = ResultType.PASSED;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
